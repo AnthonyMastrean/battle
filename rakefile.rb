206db24825bb770d142d8bd8228aacfb421870ef
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-10-30 02:58:00 -0400

    rakefile for preview and generate

---
+++ rakefile.rb
@@ -0,0 +1,10 @@
+desc "Launch the website in your browser"
+task :preview do
+  system "explorer", "index.html"
+end
+
+desc "Synchronize the website with Github Pages"
+task :deploy do |cmd|
+  git checkout gh-pages
+  git merge master
+end
